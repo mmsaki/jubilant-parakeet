bc0e2119bd54b8b228a1cd0e6b3fb34bb8c6d9f1
Meek <meek10x@gmail.com>
2023-02-21 19:09:01 -0600

    solidiy L6

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
+
+const config: HardhatUserConfig = {
+  paths: { tests: "tests" },
+  solidity: "0.8.18",
+};
+
+export default config;
